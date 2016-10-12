03a226345d23fc2b8b7102dcd1ceaca508b46faa
lonely917 <474427124@qq.com>
2016-10-12 23:06:23 +0800

    start an example on touch event

---
+++ src/com/bityan/androidskills/ViewEventTestActivity.java
@@ -0,0 +1,15 @@
+package com.bityan.androidskills;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+
+public class ViewEventTestActivity extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_view_event_test);
+	}
+}
